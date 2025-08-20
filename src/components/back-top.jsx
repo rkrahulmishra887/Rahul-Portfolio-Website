02782355b908b671bc0f680109de10e30bd5fb76
RAHUL MISHRA <rkrahulmishra88714@gmail.com>
2025-08-20 09:41:14 +0530

    updated packages to latest node

--- src/components/back-top.jsx
+++ src/components/back-top.jsx
@@ -1,30 +1,89 @@
-import React from 'react';
-import $ from 'jquery';
-import '../libs/easing.js'
+// import React from 'react';
+// import $ from 'jquery';
+// // import '../libs/easing.js'
+
+// class BackToTop extends React.Component {
+//     componentDidMount(){
+//         $('.back-to-top').click(function(){
+//             $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+//             return false;
+//           });
+//         window.addEventListener('scroll', ()=>{
+//             if(window.pageYOffset > 100){
+//                 document.querySelector('.back-to-top').classList.remove("fadeOut");
+//                 document.querySelector('.back-to-top').style.display = "block";
+//                 document.querySelector('.back-to-top').classList.add("fadeIn");
+//             }else {
+//                 document.querySelector('.back-to-top').classList.remove("fadeIn");
+//                 document.querySelector('.back-to-top').classList.add("fadeOut");
+//             }
+//         });
+//     }
+
+//     render(){
+//         return <a href="#" className="back-to-top animated"><i className="fa fa-chevron-up"></i></a>;
+//     }
+// }
+
+// export default BackToTop;
+
+import React from "react";
 
 class BackToTop extends React.Component {
-    componentDidMount(){
-        $('.back-to-top').click(function(){
-            $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-            return false;
-          });
-        window.addEventListener('scroll', ()=>{
-            if(window.pageYOffset > 100){
-                document.querySelector('.back-to-top').classList.remove("fadeOut");
-                document.querySelector('.back-to-top').style.display = "block";
-                document.querySelector('.back-to-top').classList.add("fadeIn");
-            }else {
-                document.querySelector('.back-to-top').classList.remove("fadeIn");
-                document.querySelector('.back-to-top').classList.add("fadeOut");
-            }
-        });
+  // Smooth scroll to top with easeInOutExpo easing
+  scrollToTop = () => {
+    const start = window.pageYOffset;
+    const duration = 1500;
+    let startTime = null;
+
+    // Easing function easeInOutExpo
+    function easeInOutExpo(t, b, c, d) {
+      if (t === 0) return b;
+      if (t === d) return b + c;
+      if ((t /= d / 2) < 1)
+        return (c / 2) * Math.pow(2, 10 * (t - 1)) + b;
+      return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
     }
 
-    render(){
-        return <a href="#" className="back-to-top animated"><i className="fa fa-chevron-up"></i></a>;
-    }
+    const animateScroll = (currentTime) => {
+      if (!startTime) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = easeInOutExpo(timeElapsed, start, -start, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animateScroll);
+    };
+
+    requestAnimationFrame(animateScroll);
+  };
+
+  componentDidMount() {
+    const backToTop = document.querySelector(".back-to-top");
+
+    backToTop.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.scrollToTop();
+    });
+
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 100) {
+        backToTop.classList.remove("fadeOut");
+        backToTop.style.display = "block";
+        backToTop.classList.add("fadeIn");
+      } else {
+        backToTop.classList.remove("fadeIn");
+        backToTop.classList.add("fadeOut");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <a href="#" className="back-to-top animated" style={{ display: "none" }}>
+        <i className="fa fa-chevron-up"></i>
+      </a>
+    );
+  }
 }
 
 export default BackToTop;
-
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,161 +1,289 @@
-import React from "react";
-import $ from "jquery";
+// import React from "react";
+// import $ from "jquery";
+
+// import logo1 from "../img/logo-white.jpg";
+// import logo2 from "../img/logo-blue.jpg";
+
+// class Navbar extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       logo: logo1,
+//     };
+//   }
+
+//   componentDidMount() {
+//     const nav = $("nav");
+//     let navHeight = nav.outerHeight();
+
+//     $(".navbar-toggler").on("click", function () {
+//       if (!$("#mainNav").hasClass("navbar-reduce")) {
+//         $("#mainNav").addClass("navbar-reduce");
+//       }
+//     });
+
+//     $("body").scrollspy({
+//       target: "#mainNav",
+//       offset: navHeight,
+//     });
+
+//     $(".js-scroll").on("click", function () {
+//       $(".navbar-collapse").collapse("hide");
+//     });
+
+//     window.addEventListener("scroll", () => {
+//       if (window.pageYOffset > 50) {
+//         document
+//           .querySelector(".navbar-expand-md")
+//           .classList.add("navbar-reduce");
+//         document
+//           .querySelector(".navbar-expand-md")
+//           .classList.remove("navbar-trans");
+//         this.setState({ logo: logo2 });
+//       } else {
+//         document
+//           .querySelector(".navbar-expand-md")
+//           .classList.add("navbar-trans");
+//         document
+//           .querySelector(".navbar-expand-md")
+//           .classList.remove("navbar-reduce");
+//         this.setState({ logo: logo1 });
+//       }
+//     });
+
+//     $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+//       if (
+//         window.location.pathname.replace(/^\//, "") ===
+//           this.pathname.replace(/^\//, "") &&
+//         window.location.hostname === this.hostname
+//       ) {
+//         var target = $(this.hash);
+//         target = target.length
+//           ? target
+//           : $("[name=" + this.hash.slice(1) + "]");
+//         if (target.length) {
+//           $("html, body").animate(
+//             {
+//               scrollTop: target.offset().top - navHeight + 5,
+//             },
+//             1000,
+//             "easeInExpo"
+//           );
+//           return false;
+//         }
+//       }
+//     });
+
+//     $(".js-scroll").on("click", function () {
+//       $(".navbar-collapse").collapse("hide");
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <nav
+//         className="navbar navbar-b navbar-trans navbar-expand-md fixed-top"
+//         id="mainNav"
+//       >
+//         <div className="container">
+//           <a className="navbar-brand js-scroll" href="#page-top">
+//             <img
+//               src={this.state.logo}
+//               alt="logo"
+//               style={{ maxWidth: "100px" }}
+//             />
+//           </a>
+//           <button
+//             className="navbar-toggler collapsed"
+//             type="button"
+//             data-toggle="collapse"
+//             data-target="#navbarDefault"
+//             aria-controls="navbarDefault"
+//             aria-expanded="false"
+//             aria-label="Toggle navigation"
+//           >
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//           </button>
+//           <div
+//             className="navbar-collapse collapse justify-content-end"
+//             id="navbarDefault"
+//           >
+//             <ul className="navbar-nav">
+//               <li className="nav-item">
+//                 <a className="nav-link js-scroll active" href="/">
+//                   Home
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link js-scroll" href="#about">
+//                   About
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link js-scroll" href="#work">
+//                   Work
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a
+//                   className="nav-link js-scroll"
+//                   href="/"
+//                   // href="https://drive.google.com/file/d/1BucNV5WEZxaHh9CTS1aTx38X42u1LmXb/view?usp=sharing"
+//                 >
+//                   Resume
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link js-scroll" href="/">
+//                   Testimonials
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link" href="/">
+//                   Blog
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link js-scroll" href="#contact">
+//                   Contact
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default Navbar;
+
+
+import React, { useEffect, useState } from "react";
+import { ScrollSpy, Collapse } from "bootstrap";
 
 import logo1 from "../img/logo-white.jpg";
 import logo2 from "../img/logo-blue.jpg";
 
-class Navbar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      logo: logo1,
-    };
-  }
+const Navbar = () => {
+  const [logo, setLogo] = useState(logo1);
+  const [navReduced, setNavReduced] = useState(false);
+  const [collapseInstance, setCollapseInstance] = useState(null);
 
-  componentDidMount() {
-    const nav = $("nav");
-    let navHeight = nav.outerHeight();
+  useEffect(() => {
+    const navElement = document.querySelector("nav");
+    const navHeight = navElement?.offsetHeight || 0;
 
-    $(".navbar-toggler").on("click", function () {
-      if (!$("#mainNav").hasClass("navbar-reduce")) {
-        $("#mainNav").addClass("navbar-reduce");
-      }
-    });
-
-    $("body").scrollspy({
+    // Initialize Bootstrap 5 ScrollSpy
+    const scrollSpyInstance = new ScrollSpy(document.body, {
       target: "#mainNav",
       offset: navHeight,
     });
 
-    $(".js-scroll").on("click", function () {
-      $(".navbar-collapse").collapse("hide");
-    });
+    // Setup Collapse instance for toggling menu
+    const collapseEl = document.getElementById("navbarDefault");
+    const bsCollapse = new Collapse(collapseEl, { toggle: false });
+    setCollapseInstance(bsCollapse);
 
-    window.addEventListener("scroll", () => {
+    // Scroll event to toggle navbar classes and logo
+    const handleScroll = () => {
       if (window.pageYOffset > 50) {
-        document
-          .querySelector(".navbar-expand-md")
-          .classList.add("navbar-reduce");
-        document
-          .querySelector(".navbar-expand-md")
-          .classList.remove("navbar-trans");
-        this.setState({ logo: logo2 });
+        setNavReduced(true);
+        setLogo(logo2);
       } else {
-        document
-          .querySelector(".navbar-expand-md")
-          .classList.add("navbar-trans");
-        document
-          .querySelector(".navbar-expand-md")
-          .classList.remove("navbar-reduce");
-        this.setState({ logo: logo1 });
+        setNavReduced(false);
+        setLogo(logo1);
       }
-    });
+    };
 
-    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
-      if (
-        window.location.pathname.replace(/^\//, "") ===
-          this.pathname.replace(/^\//, "") &&
-        window.location.hostname === this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top - navHeight + 5,
-            },
-            1000,
-            "easeInExpo"
-          );
-          return false;
-        }
-      }
-    });
+    window.addEventListener("scroll", handleScroll);
 
-    $(".js-scroll").on("click", function () {
-      $(".navbar-collapse").collapse("hide");
-    });
-  }
+    // Clean up event listeners on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      scrollSpyInstance.dispose();
+      bsCollapse.dispose();
+    };
+  }, []);
 
-  render() {
-    return (
-      <nav
-        className="navbar navbar-b navbar-trans navbar-expand-md fixed-top"
-        id="mainNav"
-      >
-        <div className="container">
-          <a className="navbar-brand js-scroll" href="#page-top">
-            <img
-              src={this.state.logo}
-              alt="logo"
-              style={{ maxWidth: "100px" }}
-            />
-          </a>
-          <button
-            className="navbar-toggler collapsed"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarDefault"
-            aria-controls="navbarDefault"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-          <div
-            className="navbar-collapse collapse justify-content-end"
-            id="navbarDefault"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link js-scroll active" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll" href="#work">
-                  Work
-                </a>
-              </li>
-              <li className="nav-item">
+  // Smooth scroll handler for anchor links
+  const handleSmoothScroll = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute("href").substring(1); // remove '#'
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top:
+          targetElement.getBoundingClientRect().top +
+          window.pageYOffset -
+          (document.querySelector("nav")?.offsetHeight || 0),
+        behavior: "smooth",
+      });
+    }
+    // Close navbar collapse after click
+    if (collapseInstance) {
+      collapseInstance.hide();
+    }
+  };
+
+  return (
+    <nav
+      className={`navbar navbar-b fixed-top navbar-expand-md ${
+        navReduced ? "navbar-reduce" : "navbar-trans"
+      }`}
+      id="mainNav"
+    >
+      <div className="container">
+        <a className="navbar-brand" href="#page-top" onClick={handleSmoothScroll}>
+          <img src={logo} alt="logo" style={{ maxWidth: "100px" }} />
+        </a>
+
+        <button
+          className="navbar-toggler collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarDefault"
+          aria-controls="navbarDefault"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div
+          className="navbar-collapse collapse justify-content-end"
+          id="navbarDefault"
+        >
+          <ul className="navbar-nav">
+            {[
+              { label: "Home", href: "#page-top", scroll: true },
+              { label: "About", href: "#about", scroll: true },
+              { label: "Work", href: "#work", scroll: true },
+              { label: "Resume", href: "/", scroll: false },
+              { label: "Testimonials", href: "/", scroll: false },
+              { label: "Blog", href: "/", scroll: false },
+              { label: "Contact", href: "#contact", scroll: true },
+            ].map(({ label, href, scroll }) => (
+              <li className="nav-item" key={label}>
                 <a
-                  className="nav-link js-scroll"
-                  href="/"
-                  // href="https://drive.google.com/file/d/1BucNV5WEZxaHh9CTS1aTx38X42u1LmXb/view?usp=sharing"
+                  className="nav-link"
+                  href={href}
+                  onClick={scroll ? handleSmoothScroll : undefined}
                 >
-                  Resume
+                  {label}
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll" href="/">
-                  Testimonials
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Blog
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+            ))}
+          </ul>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
+
